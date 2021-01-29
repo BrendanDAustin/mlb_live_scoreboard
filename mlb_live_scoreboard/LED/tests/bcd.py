46d5d4000d2c06ad75b42eb72d83847386d2fd52
Brendan Austin <BrendanDavidAustin@gmail.com>
2021-01-29 14:51:34 -0800

    wrote up bcd tests

--- mlb_live_scoreboard/LED/tests/bcd.py
+++ mlb_live_scoreboard/LED/tests/bcd.py
@@ -5,7 +5,7 @@ matrix = gpio_runner.matrix
 def bcd_0():
     counter = 0
     for key in matrix.LED_VALUE_MATRIX_INDICES.keys():
-        if 'top_' in key:
+        if 'top_' in key and '9' not in key and '10' not in key:
             matrix.set_inning_score(key,counter)
             counter+=1
     startTime = datetime.now()
@@ -18,7 +18,7 @@ def bcd_0():
             break
     counter = 8
     for key in matrix.LED_VALUE_MATRIX_INDICES.keys():
-        if 'top_' in key:
+        if 'top_' in key and '9' not in key and '10' not in key:
             matrix.set_inning_score(key,counter)
             counter-=1
     startTime = datetime.now()
@@ -31,7 +31,7 @@ def bcd_0():
 def bcd_1():
     counter = 0
     for key in matrix.LED_VALUE_MATRIX_INDICES.keys():
-        if 'bottom_' in key:
+        if 'bottom_' in key and '9' not in key and '10' not in key:
             matrix.set_inning_score(key,counter)
             counter+=1
     startTime = datetime.now()
@@ -44,7 +44,7 @@ def bcd_1():
             break
     counter = 8
     for key in matrix.LED_VALUE_MATRIX_INDICES.keys():
-        if 'bottom_' in key:
+        if 'bottom_' in key and '9' not in key and '10' not in key:
             matrix.set_inning_score(key,counter)
             counter-=1
     startTime = datetime.now()
@@ -53,6 +53,41 @@ def bcd_1():
         if datetime.now()>=endTime:
             break
     matrix.all_off()
+
+def bcd_2():
+    counter = 0
+    for key in matrix.LED_VALUE_MATRIX_INDICES.keys():
+        if '9' in key or '10' in key or 'runs' in key:
+            matrix.set_inning_score(key,counter)
+            counter+=1
+    startTime = datetime.now()
+    endTime = startTime + timedelta(seconds=10)
+    runner = Thread(target=gpio_runner.run)
+    runner.daemon=True
+    runner.start()
+    while True:
+        if datetime.now()>=endTime:
+            break
+    counter = 8
+    for key in matrix.LED_VALUE_MATRIX_INDICES.keys():
+        if '9' in key or '10' in key or 'runs' in key:
+            matrix.set_inning_score(key,counter)
+            counter-=1
+    startTime = datetime.now()
+    endTime = startTime + timedelta(seconds=10)
+    while True:
+        if datetime.now()>=endTime:
+            break
+    matrix.all_off()
+
+def main():
+    print('testing bcd0 (top innings 1-8)')
+    bcd_0()
+    print('testing bcd1 (bottom innings 1-8)')
+    bcd_1()
+    print("testing bcd2 (top/bottom innings 9-10, home runs, away runs)")
+    bcd_2()
+    print('done with bcd test')
 
 
     
